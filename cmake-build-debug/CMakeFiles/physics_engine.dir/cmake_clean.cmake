f63df9ea380726441b2140bb171c0200ab19b166
Neil Brags Guzman <neilbragsg@gmail.com>
2025-05-12 16:07:28 +0800

    Change the pathing to be git friendly (Added raylib.h and librarylib.a to lib)

---
+++ cmake-build-debug/CMakeFiles/physics_engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/physics_engine.dir/main.cpp.obj"
+  "CMakeFiles/physics_engine.dir/main.cpp.obj.d"
+  "libphysics_engine.dll.a"
+  "physics_engine.exe"
+  "physics_engine.exe.manifest"
+  "physics_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/physics_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
